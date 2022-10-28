d08818a6e5bdf0ebb7e7956a5e3d82b59f2c0bd5
MADZEROPIE <ask_ii1@mail.ru>
2022-10-28 19:26:37 +0300

    fixes pt.2

--- iOpt/method/method.py
+++ iOpt/method/method.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
 import numpy as np
-from typing import List
 
 from iOpt.evolvent.evolvent import Evolvent
 from iOpt.trial import Point
 from search_data import SearchData
 from search_data import SearchDataItem
 from optim_task import OptimizationTask
-from iOpt.problem import Problem
 from iOpt.solver import SolverParameters
 
 
@@ -22,20 +20,19 @@ class Method:
     min_delta: np.double = np.infty
 
     def __init__(self,
-                 problem: Problem,
                  parameters: SolverParameters,
                  task: OptimizationTask,
                  evolvent: Evolvent,
                  searchData: SearchData
                  ):
-        self.problem = problem
         self.parameters = parameters
         self.task = task
         self.evolvent = evolvent
         self.searchData = searchData
         # change to np.array, but indexing np is slower
-        self.M = [1.0] * (task.problem.numberOfObjectives)  # + task.problem.numberOfConstraints)
-        self.Z = [np.infty] * (task.problem.numberOfObjectives)  # + task.problem.numberOfConstraints)  # ???
+        self.M = [1.0 for _ in range(task.problem.numberOfObjectives + task.problem.numberOfConstraints)]
+        self.Z = [np.infty for _ in range(task.problem.numberOfObjectives + task.problem.numberOfConstraints)]
+        self.dimension = task.problem.numberOfFloatVariables  # А ДЛЯ ДИСКРЕТНЫХ?
 
     def FirstIteration(self):
         self.iterationsCount = 1
@@ -46,26 +43,28 @@ class Method:
         middle = SearchDataItem(y, x)
         left = SearchDataItem(Point(self.evolvent.GetImage(0.0), None), 0.0)
         right = SearchDataItem(Point(self.evolvent.GetImage(1.0), None), 1.0)
-        middle.SetLeft(left)  # Сделать фабрику(?) для dataitem ?
-        middle.SetRight(right)
-        left.SetRight(middle)
-        right.SetLeft(middle)
+
         left.delta = 0
-        middle.delta = 0.5
-        right.delta = 0.5
+        middle.delta = 0.5  # / self.dimension ???
+        right.delta = 0.5  # / self.dimension ???
 
         # Вычисление значения функции в 0.5
         self.CalculateFunctionals(middle)
+        self.best = middle
 
-        # Вычисление характеристик
-        recalc = True
-        self.RecalcAllCharacteristics()
-
+        # TODO: заменить на InsertFirstItem(s) и убрать SetLeft/SetRight
+        # вставить left  и right, потом middle
+        middle.SetLeft(left)
+        middle.SetRight(right)
+        left.SetRight(middle)
+        right.SetLeft(middle)
         self.searchData.InsertDataItem(right)
         self.searchData.InsertDataItem(middle, right)
         self.searchData.InsertDataItem(left, middle)
 
-        self.best = middle
+        # Вычисление характеристик
+        recalc = True
+        self.RecalcAllCharacteristics()
 
     def CheckStopCondition(self):
         self.stop = self.min_delta < self.parameters.eps
@@ -95,14 +94,15 @@ class Method:
                 dg = 1.0
 
             x = 0.5 * (left.GetX() + point.GetX())
-
             x -= 0.5 * dg * pow(abs(dif) / self.M[v], self.task.problem.numberOfFloatVariables) / self.parameters.r
 
         else:
             x = 0.5 * (point.GetX() + left.GetX())
         return x
 
-    def CalculateIterationPoints(self) -> (SearchDataItem, SearchDataItem):  # return  [(new, old)]
+    def CalculateIterationPoints(self) -> (SearchDataItem, SearchDataItem):  # return  (new, old)
+        if self.recalc is True:
+            self.RecalcAllCharacteristics()
         old = self.searchData.GetDataItemWithMaxGlobalR()
         newx = self.CalculateNextPointCoordinate(old)
         newy = self.evolvent.GetImage(newx)
@@ -135,7 +135,6 @@ class Method:
         if point.GetIndex() < 0:
             point.globalR = -np.infty
             return None
-        globalR = -np.infty
         left = point.GetLeft()
         zl = left.GetZ()
         zr = point.GetZ()
@@ -153,29 +152,28 @@ class Method:
             globalR = 2 * deltax - 4 * (zl - self.Z[v]) / (r * self.M[v])
         point.globalR = globalR
 
-    def RenewSearchData(self, points: List[(SearchDataItem, SearchDataItem)]):
+    def RenewSearchData(self, point: (SearchDataItem, SearchDataItem)):
         # calc M, R(?), insert, Z.
-        dim = self.task.problem.numberOfFloatVariables
-        for point in points:
-            self.CalculateM(point[0])
-        if self.recalc is True:
-            return
-        for point in points:
-            self.CalculateGlobalR(point[0])
-            # point[0].SetLeft(point[1].GetLeft())
-            # point[1].SetRight(point[1])
-            # point[1].SetLeft(point[0])
-            point[1].delta = pow(point[1].GetX() - point[0].GetX(), 1.0 / dim)
-            point[0].delta = pow(point[0].GetX() - point[0].GetLeft().GetX(), 1.0 / dim)
-            self.min_delta = min(point[0].delta, self.min_delta)
-            self.min_delta = min(point[1].delta, self.min_delta)
-            self.searchData.InsertDataItem(point[0], point[1])
+
+        self.CalculateM(point[0])
+
+        self.CalculateGlobalR(point[0])
+        # point[0].SetLeft(point[1].GetLeft())
+        # point[1].SetRight(point[1])
+        # point[1].SetLeft(point[0])
+        point[1].delta = pow(point[1].GetX() - point[0].GetX(), 1.0 / self.dimension)
+        point[0].delta = pow(point[0].GetX() - point[0].GetLeft().GetX(), 1.0 / self.dimension)
+        self.min_delta = min(point[0].delta, self.min_delta)
+        self.min_delta = min(point[1].delta, self.min_delta)
+        self.searchData.InsertDataItem(point[0], point[1])
 
     def UpdateOptimum(self, point: SearchDataItem):
         if self.best.GetIndex() < point.GetIndex():  # CHECK INDEX
             self.best = point
+            self.recalc = True
         elif self.best.GetIndex() == point.GetIndex() and point.GetZ() < self.best.GetZ():
             self.best = point
+            self.recalc = True
 
     def FinalizeIteration(self):
         self.iterationsCount += 1
