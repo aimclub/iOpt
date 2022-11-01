6fcf611a2799768c386ee4db6724a3afe3d084d6
YaniKolt <belovidyanochka@gmail.com>
2022-11-02 00:17:04 +0300

    main_optim_task

--- iOpt/method/optim_task.py
+++ iOpt/method/optim_task.py
@@ -5,6 +5,10 @@ from enum import Enum
 from search_data import SearchDataItem
 from iOpt.problem import Problem
 
+#УДАЛИТЬ!
+from iOpt.problems.rastrigin import Rastrigin
+from iOpt.trial import Point
+
 
 class TypeOfCalculation(Enum):
     FUNCTION = 1
@@ -14,20 +18,41 @@ class TypeOfCalculation(Enum):
 class OptimizationTask:
     def __init__(self,
                  problem: Problem,
-                 perm: np.ndarray(shape = (1), dtype = np.int)
-                ):
+                 perm: np.ndarray(shape=(1), dtype=np.int) = []
+                 ):
         self.problem = problem
-        self.perm = perm
+
+        if perm == []:
+            self.perm = np.ndarray(shape=(self.problem.numberOfFloatVariables+self.problem.numberOfDisreteVariables), dtype=np.int)
+            for i in range(self.perm.size):
+                self.perm[i]=i
+        else:
+            self.perm = perm
 
     def Calculate(self,
                   dataItem: SearchDataItem,
                   functionIndex: int,
                   type: TypeOfCalculation = TypeOfCalculation.FUNCTION
-                 ) -> SearchDataItem:
-        self.problem.Calculate(dataItem.point,
-                               dataItem.functionValues[self.perm[functionIndex]])
+                  ) -> SearchDataItem:
+        dataItem.functionValues[self.perm[functionIndex]] = self.problem.Calculate(dataItem.point,
+                                                                                   dataItem.functionValues[
+                                                                                       self.perm[functionIndex]])
+        return dataItem
         """
         Compute selected function by number.
         :return: Calculated function value.
         """
+if __name__ == "__main__":
+    ras = Rastrigin(3)
+    per = np.ndarray(shape=(1), dtype=np.int)
+    #per =[1, 0, 2]
+    #opT = OptimizationTask(ras, per)
+    opT = OptimizationTask(ras)
+    print(opT.perm[1])
+
+    point = Point([0.0, 0.0, 0.0], [])
+    sdi = SearchDataItem(point, -1, 0)
+   # sdi.functionValues = np.ndarray(shape=(1), dtype=FunctionValue)
+   # sdi[0] = FunctionValue()
+    sdi = opT.Calculate(sdi, 0)
 
