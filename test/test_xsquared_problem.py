8c89113a9d2e6f014f6debaf439759067204be42
YaniKolt <belovidyanochka@gmail.com>
2022-11-02 22:27:06 +0300

    xsquared_work

---
+++ test/test_xsquared_problem.py
@@ -0,0 +1,28 @@
+import unittest
+import numpy as np
+from iOpt.trial import FunctionValue
+from iOpt.trial import Point
+from iOpt.problems.xsquared import XSquared
+
+
+class TestXSquared(unittest.TestCase):
+    # setUp method is overridden from the parent class SearchDataItem
+    def setUp(self):
+        self.xsquared = XSquared(3)
+
+    def test_Calculate(self):
+        point = Point([1.0, 0.5, 0.3], [])
+        sum: np.double = 0
+        for i in range(self.xsquared.dimension):
+            sum += point.floatVariables[i] * point.floatVariables[i]
+
+        functionValue = FunctionValue()
+        functionValue = self.xsquared.Calculate(point, functionValue)
+        self.assertEqual(functionValue.value, sum)
+
+    def test_OptimumValue(self):
+        self.assertEqual(self.xsquared.knownOptimum[0].functionValues[0].value, 0.0)
+
+# Executing the tests in the above test case class
+if __name__ == "__main__":
+ unittest.main()
