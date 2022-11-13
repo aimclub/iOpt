8ac2e1293d30a9ae2625fcdda0ae70e595a2da14
MADZEROPIE <ask_ii1@mail.ru>
2022-11-13 14:09:07 +0300

    added paraboloid problem to make debug easier

--- test/iOpt/test_solver.py
+++ test/iOpt/test_solver.py
@@ -3,16 +3,26 @@ import unittest
 import numpy as np
 
 from iOpt.problems.rastrigin import Rastrigin
+from iOpt.problems.paraboloid import Paraboloid
 from iOpt.solver import Solver
 
 
-class TestSolveRastrigin(unittest.TestCase):
+# class TestSolveRastrigin(unittest.TestCase):
+#     def setUp(self):
+#         self.problem = Rastrigin(1)
+#         self.solver = Solver(self.problem)
+#
+#     def test_solve(self):
+#         sol = self.solver.Solve()
+#         print(sol.bestTrials)
+
+
+class TestSolveParaboloid(unittest.TestCase):
     def setUp(self):
-        self.problem = Rastrigin(1)
+        self.problem = Paraboloid(1)
         self.solver = Solver(self.problem)
 
     def test_solve(self):
         sol = self.solver.Solve()
         print(sol.bestTrials)
-
         # self.assertAlmostEqual(sol.bestTrials[0].point.floatVariables[0], self.problem.knownOptimum[0])
