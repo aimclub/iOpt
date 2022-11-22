96bdb9f1e12fd72f8bfde6bc418b55fc7de533f0
Marina Usova <usova@itmm.unn.com>
2022-11-22 13:49:59 +0300

    add some tests on paint listeners

---
+++ test/iOpt/test_paintlisteners.py
@@ -0,0 +1,37 @@
+import math
+import unittest
+import numpy as np
+
+from iOpt.problems.rastrigin import Rastrigin
+from iOpt.problems.paraboloid import Paraboloid
+from iOpt.solver import Solver
+from iOpt.solver_parametrs import SolverParameters
+from iOpt.method.listener import PaintListener, AnimationPaintListener
+
+
+class TestStaticPaintRastrigin(unittest.TestCase):
+    def setUp(self):
+        self.problem = Rastrigin(1)
+        params = SolverParameters(r=3.5, eps=0.001)
+        self.solver = Solver(self.problem, parameters=params)
+        pl = PaintListener()
+        self.solver.AddListener(pl)
+
+    def test_solveWithPrint(self):
+        sol = self.solver.Solve()
+        print(sol.bestTrials[0].point.floatVariables)
+
+class TestAnimatePaintParaboloid(unittest.TestCase):
+     def setUp(self):
+        self.problem = Paraboloid(1)
+        params = SolverParameters(r=3.5, eps=0.001)
+        self.solver = Solver(self.problem, parameters=params)
+        apl = AnimationPaintListener()
+        self.solver.AddListener(apl)
+
+     def test_solveWithPrint(self):
+         sol = self.solver.Solve()
+         print(sol.bestTrials[0].point.floatVariables)
+
+if __name__ == "__main__":
+    unittest.main()
