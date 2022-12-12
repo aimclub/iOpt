b4e7735db59b4a90aaf3eaa5a9c27b7f05a921b0
YaniKolt <belovidyanochka@gmail.com>
2022-12-13 02:13:37 +0300

    ready

--- iOpt/problems/grishagin.py
+++ iOpt/problems/grishagin.py
@@ -3,104 +3,52 @@ from iOpt.trial import Point
 from iOpt.trial import FunctionValue
 from iOpt.trial import Trial
 from iOpt.problem import Problem
-import iOpt.problems.grishagin_generation as grishaginGen
+from iOpt.problems.grishagin_function import GrishaginFunction
 import math
 
 class Grishagin(Problem):
     """Base class for optimization problems"""
 
     def __init__(self, function_number: int):
+        """
+        Конструктор класса Grishagin.
+        :param dimension: Размерность задачи = 2
+        :param functionNumber: номер задачи в наборе, :math:`1 <= functionNumber <= 100`
+        """
         self.name = Grishagin
         self.dimension = 2
         self.numberOfFloatVariables = self.dimension
         self.numberOfDisreteVariables = 0
         self.numberOfObjectives = 1
         self.numberOfConstraints = 0
-        if function_number < 1 or function_number > 100:
-            function_number = 1
-        self.fn = function_number
-        self.icnf = np.ndarray(shape = (45))
-        self.af = np.ndarray(shape=(7,7))
-        self.bf = np.ndarray(shape=(7, 7))
-        self.cf = np.ndarray(shape=(7, 7))
-        self.df = np.ndarray(shape=(7, 7))
-
-        self.floatVariableNames = np.ndarray(shape=(self.dimension), dtype=str)
+        self.floatVariableNames = np.ndarray(shape=(self.dimension,), dtype=str)
         for i in range(self.dimension):
             self.floatVariableNames[i] = i
 
-        self.lowerBoundOfFloatVariables = np.ndarray(shape=(self.dimension), dtype=np.double)
+        self.lowerBoundOfFloatVariables = np.ndarray(shape=(self.dimension,), dtype=np.double)
         self.lowerBoundOfFloatVariables.fill(0)
-        self.upperBoundOfFloatVariables = np.ndarray(shape=(self.dimension), dtype=np.double)
+        self.upperBoundOfFloatVariables = np.ndarray(shape=(self.dimension,), dtype=np.double)
         self.upperBoundOfFloatVariables.fill(1)
 
-        self.knownOptimum = np.ndarray(shape=(1), dtype=Trial)
+        self.functionNumber = function_number
+        self.function:  GrishaginFunction = GrishaginFunction(self.functionNumber)
+        self.function.SetFunctionNumber()
 
-        pointfv = np.ndarray(shape=(self.dimension), dtype=np.double)
-       # pointfv[0] = hillGen.minHill[self.fn][1]
+        self.knownOptimum = np.ndarray(shape=(1,), dtype=Trial)
+        pointfv = self.function.GetOptimumPoint()
         KOpoint = Point(pointfv, [])
-        KOfunV = np.ndarray(shape=(1), dtype=FunctionValue)
+        KOfunV = np.ndarray(shape=(1,), dtype=FunctionValue)
         KOfunV[0] = FunctionValue()
-        #KOfunV[0].value = hillGen.minHill[self.fn][0]
+        KOfunV[0] = self.Calculate(KOpoint, KOfunV[0])
         self.knownOptimum[0] = Trial(KOpoint, KOfunV)
 
     def Calculate(self, point: Point, functionValue: FunctionValue) -> FunctionValue:
-        """Compute selected function at given point."""
-        res: np.double = 0
-        #for i in range(hillGen.NUM_HILL_COEFF):
-           # res = res + hillGen.aHill[self.fn][i] * math.sin(2 * i * math.pi * point.floatVariables[0]) + hillGen.bHill[self.fn][i] * math.cos(2 * i * math.pi * point.floatVariables[0])
-        #functionValue.value = res
+        """
+        Compute selected function at given point.
+        :param point: координаты точки испытания, в которой будет вычислено значение функции
+        :param functionValue: объект определяющий номер функции в задаче и хранящий значение функции
+        :return: Вычисленное значение функции в точке point
+        """
+        functionValue.value = self.function.Calculate(point.floatVariables)
+
         return functionValue
-
-    def rndm20(self, k: np.array) -> np.double:
-        k1 = np.array([])
-        for i in range(0, 38):
-            k1[i] = k[i + 7]
-        for i in range(38, 45):
-            k1[i] = 0
-        for i in range(0, 45):
-            k[i] = abs(k[i] - k1[i])
-        for i in range(27, 45):
-            k1[i] = k[i - 27]
-        for i in range(0, 27):
-            k1[i] = 0
-
-        self.gen(k, k1, 9, 44)
-        self.gen(k, k1, 0, 8)
-
-        rndm = 0.
-        de2 = 1.
-        for i in range(0, 36):
-            de2 = de2 / 2
-            rndm = rndm + k[i + 9] * de2
-
-        return rndm
-
-    def gen(k: np.array, k1: np.array, kap1: np.int, kap2: np.int):
-        jct = 0
-        for i in reversed(kap2, kap1):
-            j = (k[i] + k1[i] + jct) / 2
-            k[i] = k[i] + k1[i] + jct - j * 2
-            jct = j
-        if jct != 0:
-            for i in reversed(kap2, kap1):
-                j = (k[i] + jct) / 2
-                k[i] = k[i] + jct - j * 2
-                jct = j
-
-    def SetFunctionNumber(self, value):
-        if value < 1 or value > 100:
-            value = 1
-        self.fn = value
-        self.icnf = np.array([])
-
-
-        nf = value
-        lst = 1
-        i1 = (nf -1)/lst
-        i2 = i1*lst
-       # for i in range(0, 45):
-
-
-
-
--- test/test_grishagin_problem.py
+++ test/test_grishagin_problem.py
@@ -3,32 +3,24 @@ import unittest
 import numpy as np
 from iOpt.trial import FunctionValue
 from iOpt.trial import Point
-from iOpt.problems.hill import Hill
-#from iOpt.problems.hill import lConstantHill
+from iOpt.problems.grishagin import Grishagin
 
 
-class TestHill(unittest.TestCase):
-    """setUp method is overridden from the parent class Rastrigin"""
+class TesGrishagin(unittest.TestCase):
+    """setUp method is overridden from the parent class Grishagin"""
     def setUp(self):
-        self.Hill = Hill(0)
+        self.Grishagin = Grishagin(4)
 
     def test_Calculate(self):
-        point = Point([0.0], [])
-        res: np.double = 0
-        aHill = np.array( [ 0.980773, -0.794122, -0.203711, 0.394696, -0.0102847, 0.53679, -0.609485, 0.870846, 0.726005, 0.00979644, -0.601245, -0.853633, 0.25248, 0.608936 ],
-                          dtype=np.double)
-        bHill = np.array( [ -0.876339, -0.629688, 0.695303, 0.00503555, 0.783258, -0.358318, 0.541063, -0.742302, 0.346843, -0.088168, 0.0708335, -0.0198675, -0.75164, 0.989074 ],
-            dtype=np.double)
-        NUM_HILL_COEFF = 14
-        for i in range(NUM_HILL_COEFF):
-            res = res + aHill[i] * math.sin(2 * i * math.pi * point.floatVariables[0]) + bHill[i] * math.cos(2 * i * math.pi * point.floatVariables[0])
+        point = Point([0.066182, 0.582587], [])
+        res: np.double = -11.541191160513788
 
         functionValue = FunctionValue()
-        functionValue = self.Hill.Calculate(point, functionValue)
+        functionValue = self.Grishagin.Calculate(point, functionValue)
         self.assertEqual(functionValue.value, res)
 
     def test_OptimumValue(self):
-        self.assertEqual(self.Hill.knownOptimum[0].functionValues[0].value, -4.8322090)
+        self.assertEqual(self.Grishagin.knownOptimum[0].functionValues[0].value, -11.541191160513788)
 
 """Executing the tests in the above test case class"""
 if __name__ == "__main__":
