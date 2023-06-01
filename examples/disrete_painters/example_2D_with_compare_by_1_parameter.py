aac84fb31ac007a4efcff0adfb75f0cc184bc8d9
Marina Usova <usova@itmm.unn.com>
2023-06-01 10:56:14 +0300

    add disrete parameters to output in console

--- examples/disrete_painters/example_2D_with_compare_by_1_parameter.py
+++ examples/disrete_painters/example_2D_with_compare_by_1_parameter.py
@@ -2,6 +2,7 @@ from problems.rastriginInt import RastriginInt
 from iOpt.solver import Solver
 from iOpt.solver_parametrs import SolverParameters
 from iOpt.output_system.listeners.static_painters import StaticDisreteListener
+from iOpt.output_system.listeners.console_outputers import ConsoleOutputListener
 
 if __name__ == "__main__":
     # create the problem 2D dimension
@@ -18,6 +19,8 @@ if __name__ == "__main__":
     solver.AddListener(apl)
     apl = StaticDisreteListener("RastriginInt_3-2_2.5_0.01_1.png", mode='bestcombination', subvars=[1, 2])
     solver.AddListener(apl)
+    cfol = ConsoleOutputListener(mode="custom")
+    solver.AddListener(cfol)
 
     # solve the problem
     sol = solver.Solve()
