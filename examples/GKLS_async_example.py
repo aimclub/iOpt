0b6e5a3da6b8062a9142eaecf34d1bdc48f786e9
oleg-w570 <ol3g.zor1n@yandex.ru>
2023-10-11 13:35:24 +0300

    pep8

--- examples/GKLS_async_example.py
+++ examples/GKLS_async_example.py
@@ -1,17 +1,19 @@
-from problems.GKLS import GKLS
+from iOpt.output_system.listeners.console_outputers import ConsoleOutputListener
 from iOpt.solver import Solver
 from iOpt.solver_parametrs import SolverParameters
-
-from iOpt.output_system.listeners.static_painters import StaticPainterNDListener
-from iOpt.output_system.listeners.console_outputers import ConsoleOutputListener
+from problems.GKLS import GKLS
 
 
 def solve_all_gkls():
     for i in range(1, 101):
         problem = GKLS(dimension=3, functionNumber=i)
-        params = SolverParameters(r=4, eps=0.01, number_of_parallel_points=4, async_scheme=True)
+        params = SolverParameters(
+            r=4, eps=0.01,
+            number_of_parallel_points=4,
+            async_scheme=True
+        )
         solver = Solver(problem=problem, parameters=params)
-        cfol = ConsoleOutputListener(mode='result')
+        cfol = ConsoleOutputListener(mode="result")
         solver.add_listener(cfol)
         solver.solve()
 
