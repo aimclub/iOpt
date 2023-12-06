6647f28140ad5ba26d3795deb5f277b96fcfc074
oleg-w570 <73493289+oleg-w570@users.noreply.github.com>
2023-12-06 17:41:57 +0300

    Асинхронная параллельная схема (#166)
    * async initial
    
    * async up
    
    * async second
    
    * pep8
    
    * pep8
    
    * switch multiprocessing to multiprocess (part of pathos)
    
    * revert gkls example
    
    * revert requirements.txt
    
    * move async implementation from async_parallel_process to async_calculator
    
    * pep8
    
    * redundant code removed
    
    * test for async parallel process
    
    * gkls async example
    add multiprocess to requirements
    
    * gkls async example
    
    * async initial
    
    * async up
    
    * async second
    
    * pep8
    
    * pep8
    
    * switch multiprocessing to multiprocess (part of pathos)
    
    * revert gkls example
    
    * revert requirements.txt
    
    * move async implementation from async_parallel_process to async_calculator
    
    * pep8
    
    * redundant code removed
    
    * test for async parallel process
    
    * gkls async example
    add multiprocess to requirements
    
    * gkls async example
    
    * semi-fix for iter-tasks

---
+++ examples/GKLS_async_example.py
@@ -0,0 +1,31 @@
+from iOpt.output_system.listeners.console_outputers import ConsoleOutputListener
+from iOpt.output_system.listeners.static_painters import StaticPainterNDListener
+from iOpt.solver import Solver
+from iOpt.solver_parametrs import SolverParameters
+from problems.GKLS import GKLS
+
+
+def solve_single_gkls():
+    """
+    Минимизация тестовой функции из GKLS генератора с номером 39
+    """
+
+    # создание объекта задачи
+    problem = GKLS(dimension=3, functionNumber=39)
+
+    # Формируем параметры решателя
+    params = SolverParameters(r=4, eps=0.01, number_of_parallel_points=4, async_scheme=True)
+
+    # Создаем решатель
+    solver = Solver(problem=problem, parameters=params)
+
+    # Добавляем вывод результатов в консоль
+    cfol = ConsoleOutputListener(mode='full')
+    solver.add_listener(cfol)
+
+    # Решение задачи
+    solver.solve()
+
+
+if __name__ == "__main__":
+    solve_single_gkls()
