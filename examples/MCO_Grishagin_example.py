315cbe9716288aeeb97e1a4a32d260787d39f996
kozinove <evgeniy.kozinov@gmail.com>
2024-03-13 19:32:47 +0300

    fix comment

--- examples/MCO_Grishagin_example.py
+++ examples/MCO_Grishagin_example.py
@@ -5,42 +5,34 @@ from iOpt.output_system.listeners.console_outputers import ConsoleOutputListener
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    """
-    """
 
-    # создание объекта задачи
     problem = Grishagin_mco(2, [3, 2])
 
-    # Формируем параметры решателя
     params = SolverParameters(r=2.5, eps=0.01, iters_limit=16000,
                               number_of_lambdas=50, start_lambdas=[[0, 1]],
-                              is_scaling=False, number_of_parallel_points=2)
+                              is_scaling=False, number_of_parallel_points=2,
+                              async_scheme=True)
 
-    # Создаем решатель
     solver = Solver(problem=problem, parameters=params)
 
-    # Добавляем вывод результатов в консоль
     cfol = ConsoleOutputListener(mode='full')
     solver.add_listener(cfol)
 
-    # Решение задачи
     sol = solver.solve()
 
-    # вывод множества Парето (координаты - значения функций)
+    # output of the Pareto set (coordinates - function values)
     var = [trial.point.float_variables for trial in sol.best_trials]
     val = [[trial.function_values[i].value for i in range(2)]for trial in sol.best_trials ]
     print("size pareto set: ", len(var))
     for fvar, fval in zip(var, val):
         print(fvar, fval)
 
-    # Точки для постороения графика множества Парето x[0]-x[1]
     x1 = [trial.point.float_variables[0] for trial in sol.best_trials]
     x2 = [trial.point.float_variables[1] for trial in sol.best_trials]
 
     plt.plot(x1, x2, 'ro')
     plt.show()
 
-    # Точки для постороения графика множества Парето y[0]-y[1]
     fv1 = [trial.function_values[0].value for trial in sol.best_trials]
     fv2 = [trial.function_values[1].value for trial in sol.best_trials]
 
