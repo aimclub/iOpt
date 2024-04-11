046376fe984434701bd65f313cfeb46c6c1ca658
LebedevIlyaG <lebedev.ilya.g@gmail.com>
2024-04-11 18:35:43 +0300

    Corrected comments

--- examples/Machine_learning/SVC/_2D/MCO_breast_cancer_example.py
+++ examples/Machine_learning/SVC/_2D/MCO_breast_cancer_example.py
@@ -7,35 +7,40 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 
+# Для построения HV индекса
+#from pymoo.util.misc import stack
+#from pymoo.indicators.hv import HV
+#import numpy as np
+
 if __name__ == "__main__":
     """
     Пробный пример многокритериальной оптимизации
     """
 
-    # загрузка датасета
+    # Загружаем датасет
     X, y = load_breast_cancer(return_X_y=True)
 
-    # разбиваем датасет на тренировочную и проверочную части
+    # Разбиваем датасет на тренировочную и проверочную части
     X_train, X_valid, y_train, y_valid = train_test_split(X, y)
 
-    # создание объекта задачи
+    # Создаем объект задачи
     problem = mco_breast_cancer(X, y, X_train, y_train)
-    # Формируем параметры решателя
 
+    # Формируем параметры решателя
     params = SolverParameters(r=3.0, eps=0.01, iters_limit=200, number_of_lambdas=50,
                               start_lambdas=[[0, 1]], is_scaling=False)
 
     # Создаем решатель
     solver = Solver(problem=problem, parameters=params)
 
-    # Добавляем вывод резултатов в консоль
+    # Добавляем вывод результатов в консоль
     cfol = ConsoleOutputListener(mode='full')
     solver.add_listener(cfol)
 
-    # Решение задачи
+    # Решаем задачу
     sol = solver.solve()
 
-    # вывод множества Парето (координаты - значения функций)
+    # Выводим множество Парето (координаты - значения функций)
     var = [trial.point.float_variables for trial in sol.best_trials]
     val = [[-trial.function_values[i].value for i in range(2)] for trial in sol.best_trials]
 
@@ -43,8 +48,26 @@ if __name__ == "__main__":
     for fvar, fval in zip(var, val):
         print(fvar, fval)
 
-    # Точки для постороения графика множества Парето y[0]-y[1]
+    # Строим график множества Парето z[0]-z[1]
     fv1 = [-trial.function_values[0].value for trial in sol.best_trials]
     fv2 = [-trial.function_values[1].value for trial in sol.best_trials]
     plt.plot(fv1, fv2, 'ro')
     plt.show()
+
+    ########################################################################
+    ## Вычисляем HV индекс
+    ########################################################################
+
+    ## Вычисляем отраженное Парето множество
+    #data = 1-np.array(val)
+    #ref_point = np.array([1.0, 1.0])
+    #print("reference point:")
+    #print(ref_point)
+    #print("Pareto set:")
+    #print(data)
+
+    ## Создаем объект для подсчета HV индекса
+    #ind = HV(ref_point=ref_point)
+
+    ## Считаем и выводим HV индекс
+    #print("HV", ind(data))
